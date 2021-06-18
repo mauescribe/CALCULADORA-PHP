5e69f520c56a84806b122fe2f2fcb36d0b835b8b
Mauro Sebastian Prystupa <81568612+mauescribe@users.noreply.github.com>
2021-06-18 18:24:23 -0300

    Add files via upload

---
+++ calculadora.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CALCULADORA</title>
+    <link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+<form class="calculadora" action="calculadora.php" method="get">
+<h1> SUMADORA PHP</h1>
+
+<input type="text" name="numero1"value="" placeholder=" ingrese un numero">
+
+<input type="text" name="numero2"value=""placeholder=" ingrese un numero">
+
+<input type="submit" name="" value="OPERAR">
+<output type='number'name=''>
+
+</form>
+</body>
+</html>
+<?php
+
+
+if
+(
+isset($_GET['numero1']) 
+&& 
+isset($_GET['numero2'])
+&& 
+is_numeric($_GET['numero1']) 
+&& 
+is_numeric($_GET['numero2'])
+)
+{
+    $numero1 = $_GET['numero1'];
+    $numero2 = $_GET['numero2'];
+    echo $numero1 + $numero2;
+};
+
+
+?>
